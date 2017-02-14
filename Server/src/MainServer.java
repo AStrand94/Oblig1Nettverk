b495cfa5883b038eae3342164470f082f1926980
Andreas Strand <strand118@hotmail.com>
2017-02-14 13:50:58 +0100

    - lagt til dusan klasser

---
+++ Server/src/MainServer.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import sun.applet.Main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by dusja on 06.02.2017.
+ */
+public class MainServer extends Application{
+    static Scene serverScene;
+    static Stage stage;
+
+
+
+    public static void main(String[] args) {
+        Application.launch(MainServer.class,(String[])null);
+    }
+
+    public void start(Stage stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("./server.fxml"));            this.stage = stage;
+            serverScene = new Scene(root, 800, 700);
+            stage.setTitle("Server");
+            stage.setScene(serverScene);
+            stage.show();
+        }catch (Exception e) {
+                Logger.getLogger(MainServer.class.getName()).log(Level.SEVERE,null,e);
+        }
+    }
+
+}
+
+
